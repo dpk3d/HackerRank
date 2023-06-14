683c9c2c0e83803ea9d269751b5c729f1e914475
Deepak Singh <39647054+dpk3d@users.noreply.github.com>
2023-06-14 23:39:45 +0530

    Write ParenthesisChecker.py

---
+++ ParenthesisChecker.py
@@ -0,0 +1,71 @@
+"""
+
+https://practice.geeksforgeeks.org/problems/parenthesis-checker2744/1
+
+Given an expression string x. Examine whether the pairs and the orders of {,},(,),[,] are correct in exp.
+For example, the function should return 'true' for exp = [()]{}{[()()]()} and 'false' for exp = [(]).
+
+Note: The drive code prints "balanced" if function return true, otherwise it prints "not balanced".
+
+Example 1:
+
+Input:
+{([])}
+Output: 
+true
+Explanation: 
+{ ( [ ] ) }. Same colored brackets can form 
+balanced pairs, with 0 number of 
+unbalanced bracket.
+
+Example 2:
+
+Input: 
+()
+Output: 
+true
+Explanation: 
+(). Same bracket can form balanced pairs, 
+and here only 1 type of bracket is 
+present and in balanced way.
+
+Example 3:
+
+Input: 
+([]
+Output: 
+false
+Explanation: 
+([]. Here square bracket is balanced but 
+the small bracket is not balanced and 
+Hence , the output will be unbalanced.
+
+
+
+"""
+  
+ # Create an empty stack to keep track of opening parentheses.
+ # Loop through each character in the input string.
+ # If the character is an opening parentheses, push it onto the stack.
+ #  If the character is a closing parentheses, pop the top element from the stack and 
+ # check whether it matches the corresponding opening parentheses for the current closing parentheses.
+ #  If the stack is empty and all parentheses have been matched, return True. Otherwise, return False.
+  
+
+  def isValid(self, s: str) -> bool:
+        stack = []
+        for char in s:
+            if char == '(' or char == '{' or char == '[':
+                stack.append(char)
+            else:
+                if not stack:
+                    return False
+                if char == ')' and stack[-1] == '(':
+                    stack.pop()
+                elif char == '}' and stack[-1] == '{':
+                    stack.pop()
+                elif char == ']' and stack[-1] == '[':
+                    stack.pop()
+                else:
+                    return False
+        return not stack
